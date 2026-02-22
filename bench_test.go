927116bf0b730f3477dea606595d2800dcdae38d
h-dav <harry.cr.davidson@gmail.com>
2026-02-22 19:05:27 +0000

    test(bench): Add complex and whitespace ParseTag benchmarks

--- bench_test.go
+++ bench_test.go
@@ -11,3 +11,19 @@ func BenchmarkParseTag_Simple(b *testing.B) {
 		_, _ = ParseTag(tag)
 	}
 }
+
+func BenchmarkParseTag_Complex(b *testing.B) {
+	tag := "PORT,required,default=8080,prefix=API_,json"
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = ParseTag(tag)
+	}
+}
+
+func BenchmarkParseTag_Whitespace(b *testing.B) {
+	tag := "  PORT  ,  required  ,  default = 8080  ,  prefix = API_  ,  json  "
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = ParseTag(tag)
+	}
+}
